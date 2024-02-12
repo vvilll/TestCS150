83907bb560f4bc8240f2af7d4ad5190f469717d9
William Wotherspoon <wtwotherspoon@nic.edu>
2024-02-12 09:20:11 -0800

    luck test

--- main.cpp
+++ main.cpp
@@ -6,6 +6,11 @@ int main()
     cout << "Testing things";
     cout << "Hello World!" << endl;
     cout << "My name is Will\n";
+    int num;
+    if (num==0)
+    {
+        cout << "that was lucky";
+    }
 
     return 0;
 }
